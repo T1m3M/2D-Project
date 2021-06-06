04ce15946a1d39ceca80bd015c63a4517bc9425e
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-06-06 11:47:41 +0200

    handling actions for menu colors

--- main.cpp
+++ main.cpp
@@ -9,6 +9,36 @@
 #include<math.h>
 #include <iostream>
 
+/* Colors */
+#define BLACK   RGB(0,0,0)
+#define WHITE   RGB(255,255,255)
+#define RED     RGB(255,0,0)
+#define ORANGE  RGB(255,128,0)
+#define YELLOW  RGB(255,255,0)
+#define GREEN   RGB(0,255,0)
+#define BLUE    RGB(0,0,255)
+#define INDIGO  RGB(75,0,130)
+#define VIOLET  RGB(143,0,255)
+
+/* Menu handles */
+#define MI_CLEAR 1001
+#define MI_SAVE 1002
+#define MI_LOAD 1003
+
+#define MI_BLACK_COLOR 1004
+#define MI_WHITE_COLOR 1005
+#define MI_RED_COLOR 1006
+#define MI_ORANGE_COLOR 1007
+#define MI_YELLOW_COLOR 1008
+#define MI_GREEN_COLOR 1009
+#define MI_BLUE_COLOR 1010
+#define MI_INDIGO_COLOR 1011
+#define MI_VIOLET_COLOR 1012
+
+# define MI_LINE_DDA 1013
+# define MI_LINE_MID 1014
+# define MI_LINE_PARA 1015
+
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 
@@ -77,14 +107,23 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     return messages.wParam;
 }
 
+/* Global variables */
+COLORREF color = BLACK;
+
 
 /*  This function is called by the Windows function DispatchMessage()  */
 
 LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    HDC hdc = GetDC(hwnd);
+
     switch (message)                  /* handle the messages */
     {
-        case WM_CREATE:
+        case WM_DESTROY:
+            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+            break;
+
+        case WM_CREATE:                        /* Create the menu bar */
         {
             HMENU hMenubar = CreateMenu();
             HMENU hFile = CreateMenu();
@@ -105,25 +144,25 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             AppendMenu(hMenubar, MF_POPUP, (UINT_PTR)hClip, "Clip");
 
             /* File sub-menus */
-            AppendMenu(hFile, MF_STRING, NULL, "Clear");
-            AppendMenu(hFile, MF_STRING, NULL, "Save");
-            AppendMenu(hFile, MF_STRING, NULL, "Load");
+            AppendMenu(hFile, MF_STRING, MI_CLEAR, "Clear");
+            AppendMenu(hFile, MF_STRING, MI_SAVE, "Save");
+            AppendMenu(hFile, MF_STRING, MI_LOAD, "Load");
 
             /* Color sub-menus */
-            AppendMenu(hColor, MF_STRING, NULL, "Black");
-            AppendMenu(hColor, MF_STRING, NULL, "White");
-            AppendMenu(hColor, MF_STRING, NULL, "Red");
-            AppendMenu(hColor, MF_STRING, NULL, "Orange");
-            AppendMenu(hColor, MF_STRING, NULL, "Yellow");
-            AppendMenu(hColor, MF_STRING, NULL, "Green");
-            AppendMenu(hColor, MF_STRING, NULL, "Blue");
-            AppendMenu(hColor, MF_STRING, NULL, "Indigo");
-            AppendMenu(hColor, MF_STRING, NULL, "Violet");
+            AppendMenu(hColor, MF_STRING, MI_BLACK_COLOR, "Black");
+            AppendMenu(hColor, MF_STRING, MI_WHITE_COLOR, "White");
+            AppendMenu(hColor, MF_STRING, MI_RED_COLOR, "Red");
+            AppendMenu(hColor, MF_STRING, MI_ORANGE_COLOR, "Orange");
+            AppendMenu(hColor, MF_STRING, MI_YELLOW_COLOR, "Yellow");
+            AppendMenu(hColor, MF_STRING, MI_GREEN_COLOR, "Green");
+            AppendMenu(hColor, MF_STRING, MI_BLUE_COLOR, "Blue");
+            AppendMenu(hColor, MF_STRING, MI_INDIGO_COLOR, "Indigo");
+            AppendMenu(hColor, MF_STRING, MI_VIOLET_COLOR, "Violet");
 
             /* Line sub-menus */
-            AppendMenu(hLine, MF_STRING, NULL, "DDA");
-            AppendMenu(hLine, MF_STRING, NULL, "Midpoint");
-            AppendMenu(hLine, MF_STRING, NULL, "parametric");
+            AppendMenu(hLine, MF_STRING, MI_LINE_DDA, "DDA");
+            AppendMenu(hLine, MF_STRING, MI_LINE_MID, "Midpoint");
+            AppendMenu(hLine, MF_STRING, MI_LINE_PARA, "parametric");
 
             /* Circle sub-menus */
             AppendMenu(hCircle, MF_STRING, NULL, "Direct");
@@ -147,12 +186,87 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             AppendMenu(hClip, MF_STRING, NULL, "Line");
 
             SetMenu(hwnd, hMenubar);
+
             break;
         }
 
-        case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-            break;
+        case WM_COMMAND:                /* bind menus to actions */
+        {
+            switch (LOWORD(wParam))
+            {
+                case MI_CLEAR:
+                {
+                    /* Write clear code here */
+                    std::cout << "Data is cleared!\n\n";
+                    break;
+                }
+
+                case MI_SAVE:
+                {
+                    std::cout << "Saving the data...\n";
+                    /* Write save code here */
+                    std::cout << "Data is saved!\n\n";
+                    break;
+                }
+
+                case MI_LOAD:
+                {
+                    std::cout << "Loading the data...\n";
+                    /* Write load code here */
+                    std::cout << "Data is loaded!\n\n";
+                    break;
+                }
+
+                case MI_BLACK_COLOR:
+                {
+                    color = BLACK;
+                    std::cout << "Color changed to black!\n\n"; break;
+                }
+                case MI_WHITE_COLOR:
+                {
+                    color = WHITE;
+                    std::cout << "Color changed to white!\n\n"; break;
+                }
+                case MI_RED_COLOR:
+                {
+                    color = RED;
+                    std::cout << "Color changed to red!\n\n"; break;
+                }
+                case MI_ORANGE_COLOR:
+                {
+                    color = ORANGE;
+                    std::cout << "Color changed to orange!\n\n"; break;
+                }
+                case MI_YELLOW_COLOR:
+                {
+                    color = YELLOW;
+                    std::cout << "Color changed to yellow!\n\n"; break;
+                }
+                case MI_GREEN_COLOR:
+                {
+                    color = GREEN;
+                    std::cout << "Color changed to green!\n\n"; break;
+                }
+                case MI_BLUE_COLOR:
+                {
+                    color = BLUE;
+                    std::cout << "Color changed to blue!\n\n"; break;
+                }
+                case MI_INDIGO_COLOR:
+                {
+                    color = INDIGO;
+                    std::cout << "Color changed to indigo!\n\n"; break;
+                }
+                case MI_VIOLET_COLOR:
+                {
+                    color = VIOLET;
+                    std::cout << "Color changed to violet!\n\n"; break;
+                }
+
+                default:
+                    break;
+            }
+        }
         default:                      /* for messages that we don't deal with */
             return DefWindowProc (hwnd, message, wParam, lParam);
     }
