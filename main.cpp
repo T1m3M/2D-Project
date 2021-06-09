8bf377215179552478da0a2241bb1fc71266f1d9
Abd-Elrahman-Nasr <abdelrahman_nasr99@hotmail.com>
2021-06-09 16:16:39 +0200

    Finishing clipping integration

--- main.cpp
+++ main.cpp
@@ -142,6 +142,8 @@ int click_count = 0;
 
 int r;
 int x_1,y_1,x_2,y_2;
+int X_left, Y_top, X_right, Y_bottom;
+int X_start, Y_start, X_end, Y_end;
 
 /// Circle.
 
@@ -504,6 +506,7 @@ void fillQ1(HDC hdc,int x,int y,int xc,int yc,COLORREF color)
     SetPixel(hdc, xc+y, yc-x, color);
     parametric(hdc, xc, yc, xc+y, yc-x,  color);
 }
+
 void fillQ2(HDC hdc,int x,int y,int xc,int yc,COLORREF color)
 {
     SetPixel(hdc, xc-x, yc-y, color);
@@ -511,6 +514,7 @@ void fillQ2(HDC hdc,int x,int y,int xc,int yc,COLORREF color)
     SetPixel(hdc, xc-y, yc-x, color);
     parametric(hdc, xc, yc, xc-y, yc-x,  color);
 }
+
 void fillQ3(HDC hdc,int x,int y,int xc,int yc,COLORREF color)
 {
     SetPixel(hdc, xc-x, yc+y, color);
@@ -518,6 +522,7 @@ void fillQ3(HDC hdc,int x,int y,int xc,int yc,COLORREF color)
     SetPixel(hdc, xc-y, yc+x, color);
     parametric(hdc, xc, yc, xc-y, yc+x,  color);
 }
+
 void fillQ4(HDC hdc,int x,int y,int xc,int yc,COLORREF color)
 {
     SetPixel(hdc, xc+x, yc+y, color);
@@ -550,6 +555,99 @@ void filling_algo(HDC hdc,int xc,int yc,int x1, int y1,int q, COLORREF color)
             fillQ3(hdc,x,y,xc,yc,color);
         else
             fillQ4(hdc,x,y,xc,yc,color);
+    }
+}
+
+
+/// Point clipping
+void clip_point_algo(HDC hdc, int x,int y,int xleft,int ytop,int xright,int ybottom,COLORREF color)
+{
+if(x>=xleft && x<= xright && y>=ytop && y<=ybottom)
+
+SetPixel(hdc,x,y,color);
+}
+
+/// line clipping for a rectangle shape
+// union structure for the 4 bits
+union OutCode
+{
+    unsigned All:4;
+    struct
+    {
+        unsigned left:1,top:1,right:1,bottom:1;
+    };
+};
+
+OutCode GetOutCode(double x,double y,int xleft,int ytop,int xright,int ybottom)
+{
+    OutCode obj;
+    obj.All = 0;
+    if(x > xright)
+      obj.right = 1;
+    else if(x < xleft)
+      obj.left = 1;
+    if(y < ytop)
+      obj.top = 1;
+    else if(y > ybottom)
+      obj.bottom = 1;
+    return obj;
+}
+
+void VIntersect(double xs,double ys,double xe,double ye,int x,double *xi,double *yi)
+{
+    *xi=x;
+    *yi=ys+(x-xs)*(ye-ys)/(xe-xs);
+}
+
+void HIntersect(double xs,double ys,double xe,double ye,int y,double *xi,double *yi)
+{
+    *yi=y;
+    *xi=xs+(y-ys)*(xe-xs)/(ye-ys);
+}
+
+void clip_line_algo(HDC hdc,int xs,int ys,int xe,int ye,int xleft,int ytop,int xright,int ybottom)
+{
+    double x1=xs, y1=ys, x2=xe, y2=ye;
+    OutCode out1 = GetOutCode(x1,y1,xleft,ytop,xright,ybottom);
+    OutCode out2 = GetOutCode(x2,y2,xleft,ytop,xright,ybottom);
+    while( (out1.All || out2.All) && !(out1.All & out2.All))
+    {
+        double xi,yi;
+        if(out1.All)
+        {
+            if(out1.left)
+                VIntersect(x1,y1,x2,y2,xleft,&xi,&yi);
+            else if(out1.top)
+                HIntersect(x1,y1,x2,y2,ytop,&xi,&yi);
+            else if(out1.right)
+                VIntersect(x1,y1,x2,y2,xright,&xi,&yi);
+            else
+                HIntersect(x1,y1,x2,y2,ybottom,&xi,&yi);
+            x1=xi;
+            y1=yi;
+            out1=GetOutCode(x1,y1,xleft,ytop,xright,ybottom);
+        }
+        else
+        {
+            if(out2.left)
+                VIntersect(x1,y1,x2,y2,xleft,&xi,&yi);
+            else if(out2.top)
+                HIntersect(x1,y1,x2,y2,ytop,&xi,&yi);
+            else if(out2.right)
+                VIntersect(x1,y1,x2,y2,xright,&xi,&yi);
+            else
+                HIntersect(x1,y1,x2,y2,ybottom,&xi,&yi);
+            x2=xi;
+            y2=yi;
+            out2=GetOutCode(x2,y2,xleft,ytop,xright,ybottom);
+        }
+    }
+    if(!out1.All && !out2.All)
+    {
+
+        //MoveToEx(hdc,round(x1),round(y1),NULL);
+        //LineTo(hdc,round(x2),round(y2));
+        parametric(hdc, round(x1), round(y1), round(x2), round(y2), color);
     }
 }
 
@@ -860,16 +958,24 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                     if (click_count == 0)
                     {
                         // code for start point of rectangular
+                        X_left=x;
+                        Y_top=y;
                         click_count++;
                     }
                     else if (click_count == 1)
                     {
                         // code for end point of rectangular
+                        X_right=x;
+                        Y_bottom=y;
+                        Rectangle(hdc, X_left,Y_top,X_right,Y_bottom);
                         click_count++;
                     }
                     else
                     {
                         // code for points
+                        x_1=x;
+                        y_1=y;
+                        clip_point_algo(hdc, x_1, y_1, X_left,Y_top,X_right,Y_bottom, color);
                         // resets by default when changing the algorithm (many points)
                     }
                     break;
@@ -880,18 +986,28 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                     if (click_count == 0)
                     {
                         // code for start point of rectangular
+                        X_left=x;
+                        Y_top=y;
                     }
                     else if (click_count == 1)
                     {
                         // code for end point of rectangular
+                        X_right=x;
+                        Y_bottom=y;
+                        Rectangle(hdc, X_left,Y_top,X_right,Y_bottom);
                     }
                     else if (click_count % 2 == 0)
                     {
                         // code for start point of lines
+                        X_start=x;
+                        Y_start=y;
                     }
                     else
                     {
                         // code for end point of lines
+                        X_end=x;
+                        Y_end=y;
+                        clip_line_algo(hdc,X_start,Y_start,X_end,Y_end,X_left,Y_top,X_right,Y_bottom);
                         // resets by default when changing the algorithm (many lines)
                     }
 
